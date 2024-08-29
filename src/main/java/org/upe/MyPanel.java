aa701bd5057b3ac934cda4d40d971743d1309d38
Keisy <k22.lizandra@gmail.com>
2024-08-29 18:39:17 -0300

    add classes

---
+++ src/main/java/org/upe/MyPanel.java
@@ -0,0 +1,57 @@
+package org.upe;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java. io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class MyPanel extends JPanel {
+    private static final long serialVersionUID = 5433149762760327082L;
+
+    private BufferedImage logo;
+
+    public MyPanel() {
+
+        this.setPreferredSize(new Dimension(800, 600));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        logo = loadImage("data/bpb.png", 100, 100);
+    }
+
+    private BufferedImage loadImage(String imagePath, int width, int height) {
+
+        BufferedImage scaledImage = null;
+        try {
+            // load image
+            File imgFile = new File(imagePath);
+            BufferedImage image = ImageIO.read(imgFile);
+            // scale image
+            scaledImage = new BufferedImage(width, height, image.getType());
+
+            Graphics2D g2 = scaledImage.createGraphics( );
+            g2.drawImage(image, 0, 0, width, height, null);
+            g2.dispose();
+
+        } catch (IOException ex) {
+            ex.printStackTrace( );
+        }
+
+        return scaledImage;
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(Color.CYAN);
+        g2.drawLine(100, 100, 700, 500);
+
+        g2.setColor(Color.MAGENTA);
+        g2.drawLine(100, 100, 700, 600);
+    }
+
+}
--- src/main/java/org/upe/SimpleDraw.java
+++ src/main/java/org/upe/SimpleDraw.java
@@ -9,5 +9,10 @@ public class SimpleDraw
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         frame.setTitle("Simple Drawings");
+
+        MyPanel panel = new MyPanel();
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
