15e8c880ce0a1fd6d20471611fd198a68be0bccf
Keisy <k22.lizandra@gmail.com>
2024-08-29 16:38:22 -0300

    add files

---
+++ src/main/java/org/upe/SimpleDraw.java
@@ -0,0 +1,13 @@
+package org.upe;
+import javax.swing.JFrame;
+
+
+public class SimpleDraw
+{
+    public static void main( String[] args )
+    {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setTitle("Simple Drawings");
+    }
+}
